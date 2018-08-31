e102255963c58cd3b14bddaa0be08c0b66fccfeb
Yaasai <santana.lucasg@gmail.com>
2018-08-31 01:21:25 -0300

    Adicionado o primeiro código para main.c

--- main.c
+++ main.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
+#include <allegro5/allegro.h>
 
-main(){
+#define JANELA_X 1280
+#define JANELA_Y 720
+
+int main(){
     
-    
+    al_init();
+
+    ALLEGRO_DISPLAY *janela = al_create_display(JANELA_X,JANELA_Y);
+    ALLEGRO_COLOR branco = al_map_rgb(255,255,255);
+
+    al_clear_to_color(branco);
+    al_flip_display();
+
+    al_rest(5.0);
+    al_destroy_display(janela);
 
     return 0;
 }
