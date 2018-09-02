b880ed3a024a1f76f2735bf5f16f4e5f5b5dbec7
Yaasai <santana.lucasg@gmail.com>
2018-09-02 00:48:38 -0300

    Adicionando o código da imagem com zoom

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <allegro5/allegro.h>
+#include <allegro5/allegro_image.h>
 
 #define JANELA_X 1280
 #define JANELA_Y 720
@@ -7,11 +8,15 @@
 int main(){
     
     al_init();
+    al_init_image_addon();
 
+    ALLEGRO_BITMAP *imagem = al_load_bitmap("docs/images/lizardPixelArt.png");
     ALLEGRO_DISPLAY *janela = al_create_display(JANELA_X,JANELA_Y);
     ALLEGRO_COLOR branco = al_map_rgb(255,255,255);
 
+
     al_clear_to_color(branco);
+    al_draw_scaled_bitmap(imagem,0,0,23,38,500,250,115,190,0);
     al_flip_display();
 
     al_rest(5.0);
