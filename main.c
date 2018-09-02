7fc79e48e3edbff576e06d52b5d48ec2ef95f6e2
Juan Suman <victor.suman23@gmail.com>
2018-09-02 03:12:34 -0300

    Adicionada a primeira versão do Menu Principal.

--- main.c
+++ main.c
@@ -6,20 +6,20 @@
 #define JANELA_Y 720
 
 int main(){
-    
+
     al_init();
     al_init_image_addon();
 
     ALLEGRO_BITMAP *imagem = al_load_bitmap("docs/images/lizardPixelArt.png");
     ALLEGRO_DISPLAY *janela = al_create_display(JANELA_X,JANELA_Y);
-    ALLEGRO_COLOR branco = al_map_rgb(255,255,255);
+    ALLEGRO_COLOR branco = al_map_rgb(255, 255, 255);
 
 
     al_clear_to_color(branco);
-    al_draw_scaled_bitmap(imagem,0,0,23,38,500,250,115,190,0);
+    al_draw_scaled_bitmap(imagem, 0, 0, 23, 38, 500, 250, 115, 190, 0);
     al_flip_display();
 
-    al_rest(5.0);
+    al_rest(05.0);
     al_destroy_display(janela);
 
     return 0;
