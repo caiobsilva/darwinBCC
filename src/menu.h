50d844fe53bffcf28f6a870fc3a018565064fa62
Juan Suman <victor.suman23@gmail.com>
2018-09-11 01:58:48 -0300

    Organização do código.

--- src/menu.h
+++ src/menu.h
@@ -2,57 +2,74 @@
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_image.h>
 
-int desenhar(int n){
+int selectMenu(ALLEGRO_EVENT_QUEUE *fila, ALLEGRO_EVENT evento){
+    int a = 0;
 
-    ALLEGRO_BITMAP *background = al_load_bitmap("../res/images/introMenu01.png");
-    ALLEGRO_BITMAP *background1 = al_load_bitmap("../res/images/introMenu02.png");
-    ALLEGRO_BITMAP *background2 = al_load_bitmap("../res/images/introMenu03.png");
+    ALLEGRO_BITMAP *select[2];
 
+    ALLEGRO_COLOR branco = al_map_rgb(255, 255, 255);
+    
+    select[0] = al_load_bitmap("../res/images/selectMenu1.png");
+    select[1] = al_load_bitmap("../res/images/selectMenu2.png");
+    
+    while(a < 4){
+        al_clear_to_color(branco);
 
-    switch (n){
-        case 1:
-        al_draw_scaled_bitmap(background, 0, 0, 896, 504, 0, 0, 1280, 720, 0);
-        break;
-        case 2:
-        al_draw_scaled_bitmap(background1, 0, 0, 896, 504, 0, 0, 1280, 720, 0);
-        break;
-        case 3:
-        al_draw_scaled_bitmap(background2, 0, 0, 896, 504, 0, 0, 1280, 720, 0);
-        break;
+        for (int n = 0; n < 2; n++){
+            al_draw_scaled_bitmap(select[n], 0, 0, 896, 504, 0, 0, 1280, 720, 0);
+
+            if(n > 2){
+                n = 1;
+            }
+            al_flip_display();
+            al_rest(1);
+        }
+        a++;
     }
+    
+    al_destroy_bitmap(select[0]);
+    al_destroy_bitmap(select[1]);
 
-    al_destroy_bitmap(background);
-    al_destroy_bitmap(background1);
-    al_destroy_bitmap(background2);
+    return 0;
+}
+
+int introMenu(int n){
+
+    ALLEGRO_BITMAP *intro[4];
+
+    intro[0] = al_load_bitmap("../res/images/introMenu1.png");
+    intro[1] = al_load_bitmap("../res/images/introMenu2.png");
+    intro[2] = al_load_bitmap("../res/images/introMenu3.png");
+    intro[3] = al_load_bitmap("../res/images/introMenu4.png");
+
+    al_draw_scaled_bitmap(intro[n], 0, 0, 896, 504, 0, 0, 1280, 720, 0);
+
+    al_destroy_bitmap(intro[0]);
+    al_destroy_bitmap(intro[1]);
+    al_destroy_bitmap(intro[2]);
+    al_destroy_bitmap(intro[3]);
 
     return (n += 1);
 }
 
 int menu(ALLEGRO_EVENT_QUEUE *fila, ALLEGRO_EVENT evento){
-
-    int n = 1;
+    int n = 0;
 
     ALLEGRO_COLOR branco = al_map_rgb(255, 255, 255);
-    ALLEGRO_COLOR preto = al_map_rgb(0,0,0);
 
-    while(1){
-        
+    while(!evento.type){
         al_wait_for_event_timed(fila, &evento, 0.05);
-        if(evento.type){
-            break;
-        }
         al_clear_to_color(branco);
-        n = desenhar(n);
+        
+        n = introMenu(n);
         al_flip_display();
-        al_rest(0.5);
+        al_rest(.5);
 
         if(n > 3){
-            n = 1;
+            n = 0;
         }        
     }
-
-    al_clear_to_color(preto);
-    al_flip_display();
+    selectMenu(fila, evento);
 
     return 0;
 }
