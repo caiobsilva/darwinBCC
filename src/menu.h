7043af5cf29855b5c171f4483d993b6252497456
Yaasai <santana.lucasg@gmail.com>
2018-09-12 00:01:11 -0300

    Versão pre-merge

--- src/menu.h
+++ src/menu.h
@@ -2,37 +2,6 @@
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_image.h>
 
-int selectMenu(ALLEGRO_EVENT_QUEUE *fila, ALLEGRO_EVENT evento){
-    int a = 0;
-
-    ALLEGRO_BITMAP *select[2];
-
-    ALLEGRO_COLOR branco = al_map_rgb(255, 255, 255);
-
-    select[0] = al_load_bitmap("../res/images/selectMenu1.png");
-    select[1] = al_load_bitmap("../res/images/selectMenu2.png");
-
-    while (a < 4){
-        al_clear_to_color(branco);
-
-        for (int n = 0; n < 2; n++){
-            al_draw_scaled_bitmap(select[n], 0, 0, 896, 504, 0, 0, 1280, 720, 0);
-
-            if (n > 2){
-                n = 1;
-            }
-            al_flip_display();
-            al_rest(1);
-        }
-        a++;
-    }
-
-    al_destroy_bitmap(select[0]);
-    al_destroy_bitmap(select[1]);
-
-    return 0;
-}
-
 int introMenu(int n){
 
     ALLEGRO_BITMAP *intro[4];
@@ -42,13 +11,13 @@ int introMenu(int n){
     intro[2] = al_load_bitmap("../res/images/introMenu3.png");
     intro[3] = al_load_bitmap("../res/images/introMenu4.png");
 
-    if(n < 10){
+    if(n < 6){
         al_draw_scaled_bitmap(intro[0], 0, 0, 896, 504, 0, 0, 1280, 720, 0);
-    }else if(n < 20){
+    }else if(n < 12){
         al_draw_scaled_bitmap(intro[1], 0, 0, 896, 504, 0, 0, 1280, 720, 0);
-    }else if(n < 30){
+    }else if(n < 18){
         al_draw_scaled_bitmap(intro[2], 0, 0, 896, 504, 0, 0, 1280, 720, 0);
-    }else if(n <= 40){
+    }else if(n <= 24){
         al_draw_scaled_bitmap(intro[3], 0, 0, 896, 504, 0, 0, 1280, 720, 0);
     }
     
@@ -61,18 +30,23 @@ int introMenu(int n){
 }
 
 int menu(ALLEGRO_EVENT_QUEUE *fila, ALLEGRO_EVENT evento){
-    int n = 1;
+    int sair = 0, n = 1;
 
     ALLEGRO_COLOR branco = al_map_rgb(255, 255, 255);
 
-    while (!evento.type){
-    
+    while (!sair){
+        
         al_wait_for_event_timed(fila, &evento, 0.05);
+        
+        if(evento.type == ALLEGRO_EVENT_KEY_DOWN){
+            sair = 1;
+        }
+
         al_clear_to_color(branco);
         n = introMenu(n);
         al_flip_display();  
 
-        if(n > 40){
+        if(n > 24){
             n = 1;
         }
 
