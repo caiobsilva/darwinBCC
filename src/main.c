4a6201ebefb18a734c7a88ccc9a539d947b1fd62
Yaasai <santana.lucasg@gmail.com>
2018-10-29 17:56:09 -0300

    Comentários adicionados no menu e na main

--- src/main.c
+++ src/main.c
@@ -7,15 +7,28 @@
 #define JANELA_X 1280
 #define JANELA_Y 720
 
+/*
+========= YOSTER ISLAND =============
+Caio Braga
+Juan Soman
+Lucas Gomes
+
+Jogo educativo sobre a teoria da evolução criado com
+o pacote ALLEGRO na linguagem C.
+
+*/
+
+
 int main(){
     enum statesGame selecaoTela = Menu;
 
+    //INICIANDO ADDONS E INSTALANDO ELEMENTOS NECESSÁRIOS
     al_init();
     al_init_image_addon();
+    al_init_acodec_addon();
     al_install_keyboard();
     al_install_mouse();
     al_install_audio();
-    al_init_acodec_addon();
 
     ALLEGRO_BITMAP *cursorSprite = al_load_bitmap("../res/images/cursor.png");
     ALLEGRO_DISPLAY *janela = al_create_display(JANELA_X, JANELA_Y);
@@ -29,6 +42,7 @@ int main(){
     ALLEGRO_MOUSE_CURSOR *cursor = al_create_mouse_cursor(cursorSprite, 20, 20);
     al_set_mouse_cursor(janela, cursor);
 
+    // Loop de seleção de tela (Jogo)
     while(selecaoTela != Exit){
         switch(selecaoTela){
             case Menu:
--- src/menu.c
+++ src/menu.c
@@ -4,14 +4,17 @@
 #include <allegro5/allegro_audio.h>
 #include "header.h"
 
+//Intro do menu
 int menuIntro(int n){
     ALLEGRO_BITMAP *intro[4];
 
+    //Declarando os quatro frames da intro do menu
     intro[0] = al_load_bitmap("../res/images/introMenu1.png");
     intro[1] = al_load_bitmap("../res/images/introMenu2.png");
     intro[2] = al_load_bitmap("../res/images/introMenu3.png");
     intro[3] = al_load_bitmap("../res/images/introMenu4.png");
 
+    //Decidindo qual dos frames vai ser desenhado
     if(n < 6){
         al_draw_scaled_bitmap(intro[0], 0, 0, 896, 504, 0, 0, 1280, 720, 0);
     }
@@ -32,11 +35,13 @@ int menuIntro(int n){
     return (n += 1);
 }
 
+// Essa é a função do primeiro menu própriamente dita.
 enum statesGame menu(ALLEGRO_EVENT_QUEUE *fila, ALLEGRO_EVENT evento){
     int sair = 0, n = 1;
 
     ALLEGRO_COLOR branco = al_map_rgb(255, 255, 255);
-
+    
+    //Loop do menu
     while (!sair){
         al_wait_for_event_timed(fila, &evento, 0.05);
 
@@ -48,9 +53,13 @@ enum statesGame menu(ALLEGRO_EVENT_QUEUE *fila, ALLEGRO_EVENT evento){
         }
 
         al_clear_to_color(branco);
+        // A variavel n é igual ao retorno da função de densenho
+        // Sempre aumentando de 1 em 1
         n = menuIntro(n);
         al_flip_display();
 
+        // Como cada frame toma 6 cliclos e tem 4, quando o n for 25
+        // ele volta para o primeiro frame.
         if(n > 24){
             n = 1;
         }
@@ -59,7 +68,8 @@ enum statesGame menu(ALLEGRO_EVENT_QUEUE *fila, ALLEGRO_EVENT evento){
     return Select;
 }
 
-
+// Depois que uma tecla for apertada o jogador seguirá para o menu de "Novo jogo", essa é a função que
+// desenha o menu e encaminha para a opção escolhida.
 enum statesGame menuSelect(ALLEGRO_EVENT_QUEUE *fila, ALLEGRO_EVENT evento){
     enum statesGame estado;
     int sair = 0;
@@ -71,7 +81,7 @@ enum statesGame menuSelect(ALLEGRO_EVENT_QUEUE *fila, ALLEGRO_EVENT evento){
     ALLEGRO_SAMPLE *musicaMenu = al_load_sample("../res/audio/menuTheme.flac");
 
     ALLEGRO_BITMAP *arraySelecionar[3];
-
+    
     arraySelecionar[0] = al_load_bitmap("../res/images/selectMenu1.png");
     arraySelecionar[1]  = al_load_bitmap("../res/images/selectMenuNewGame.png");
     arraySelecionar[2]  = al_load_bitmap("../res/images/selectMenuExit.png");
@@ -90,17 +100,20 @@ enum statesGame menuSelect(ALLEGRO_EVENT_QUEUE *fila, ALLEGRO_EVENT evento){
             return Exit;
         }
         if(evento.type == ALLEGRO_EVENT_KEY_DOWN){
+            //Se o jogador apertar a "W" ou a seta para cima a opção de novo jogo será desenhada
             if(evento.keyboard.keycode == ALLEGRO_KEY_UP || evento.keyboard.keycode == ALLEGRO_KEY_W){
                 al_clear_to_color(branco);
                 al_draw_scaled_bitmap(arraySelecionar[1], 0, 0, 896, 504, 0, 0, 1280, 720, 0);
                 al_play_sample(efeitoTrocar, 1.0, 0.0, 1.0, ALLEGRO_PLAYMODE_ONCE, NULL);
                 estado = AmbienteUm;
             }if(evento.keyboard.keycode == ALLEGRO_KEY_DOWN || evento.keyboard.keycode == ALLEGRO_KEY_S){
+            //Se o jogador apertar a "S" ou a seta para baixo a opção de sair do jogo será desenhada
                 al_clear_to_color(branco);
                 al_draw_scaled_bitmap(arraySelecionar[2], 0, 0, 896, 504, 0, 0, 1280, 720, 0);
                 al_play_sample(efeitoTrocar, 1.0, 0.0, 1.0, ALLEGRO_PLAYMODE_ONCE, NULL);
                 estado = Exit;
             }else if(evento.keyboard.keycode == ALLEGRO_KEY_ENTER || evento.keyboard.keycode == ALLEGRO_KEY_SPACE){
+            //Se o jogador apertar a "espaço" ou a "ENTER" a opção selecionada vai ser computada
                 if(estado != Menu){
                     al_play_sample(efeitoSelecionar, 1.0, 0.0, 1.0, ALLEGRO_PLAYMODE_ONCE, NULL);
                     al_rest(0.8);
