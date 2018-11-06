0536967ecc8135bfa3b14670efa22fa38364594d
Caio <caiobs.99@gmail.com>
2018-11-06 11:50:04 -0200

    alteração nas falas para apresentar o vulcão

--- src/falas.c
+++ src/falas.c
@@ -102,7 +102,7 @@ int falas(ALLEGRO_EVENT_QUEUE *fila, ALLEGRO_EVENT evento, int s, int x, int y, 
     falasInteracoes[3] = "O desenvolvimento pode ser atrasado ou adiantado com a intervenção de outro ser. Chamamos isso de 'Interações Ambientais'";
 
     char *falasNinho[3];
-    falasNinho[0] = "Um ninho! Era disso que precisavamos!"; 
+    falasNinho[0] = "Um ninho! Era disso que precisávamos, e este vulcão parece dar a proteção ideal contra ataques de outras espécies."; 
     falasNinho[1] = "É importante para o desenvolvimento de uma espécie que ela tenha um lugar seguro para se reproduzir";
     falasNinho[2] = "É durante a troca de geração que as novas característica podem ser  desenvolvidas, com sorte ganharemos uma que se encaixa bem no nosso ambiente";
     
