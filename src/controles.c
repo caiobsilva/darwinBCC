4563d52778fc4c94d60d6cdaaa8a4aad1abf366c
Caio <caiobs.99@gmail.com>
2018-12-01 21:41:58 -0200

    flagVida

--- src/controles.c
+++ src/controles.c
@@ -1,7 +1,7 @@
 #include <allegro5/allegro.h>
 #include "header.h"
 
-void movimentacao(ALLEGRO_EVENT evento,tile *tileAtual, int *x, int *y, int *t, int flagEvolucao){
+void movimentacao(ALLEGRO_EVENT evento,tile *tileAtual, int *x, int *y, int *t, int flagEvolucao, int *flagVida){
     ALLEGRO_KEYBOARD_STATE tecla;
     al_get_keyboard_state(&tecla);
     static int i;
@@ -37,7 +37,7 @@ void movimentacao(ALLEGRO_EVENT evento,tile *tileAtual, int *x, int *y, int *t, 
 
     if(al_key_down(&tecla, ALLEGRO_KEY_W) || al_key_down(&tecla, ALLEGRO_KEY_UP)){
         *y -= 5;
-        colisao(tileAtual, x, y, 1);
+        colisao(tileAtual, x, y, 1, flagVida);
         if(*t <= 6){
             al_draw_scaled_bitmap(player[3], 0, 0, 22, 22, *x, *y, 66, 66, 0);
         }else if(*t <= 12){
@@ -49,7 +49,7 @@ void movimentacao(ALLEGRO_EVENT evento,tile *tileAtual, int *x, int *y, int *t, 
         *t += 1;
     }else if(al_key_down(&tecla, ALLEGRO_KEY_S) || al_key_down(&tecla, ALLEGRO_KEY_DOWN)){
         *y += 5;
-        colisao(tileAtual, x, y, 2);
+        colisao(tileAtual, x, y, 2, flagVida);
         if(*t <= 6){
             al_draw_scaled_bitmap(player[0], 0, 0, 22, 22, *x, *y, 66, 66, 0);
         }else if(*t <= 12){
@@ -61,7 +61,7 @@ void movimentacao(ALLEGRO_EVENT evento,tile *tileAtual, int *x, int *y, int *t, 
         *t += 1;
     }else if(al_key_down(&tecla, ALLEGRO_KEY_D) || al_key_down(&tecla, ALLEGRO_KEY_RIGHT)){
         *x += 5;
-        colisao(tileAtual, x, y, 3);
+        colisao(tileAtual, x, y, 3, flagVida);
         if(*t <= 6){
             al_draw_scaled_bitmap(player[9], 0, 0, 22, 22, *x, *y, 66, 66, 0);
         }else if(*t <= 12){
@@ -73,7 +73,7 @@ void movimentacao(ALLEGRO_EVENT evento,tile *tileAtual, int *x, int *y, int *t, 
         *t += 1;
     }else if(al_key_down(&tecla, ALLEGRO_KEY_A) || al_key_down(&tecla, ALLEGRO_KEY_LEFT)){
         *x -= 5;
-        colisao(tileAtual, x, y, 4);
+        colisao(tileAtual, x, y, 4, flagVida);
         if(*t <= 6){
             al_draw_scaled_bitmap(player[6], 0, 0, 22, 22, *x, *y, 66, 66, 0);
         }else if(*t <= 12){
